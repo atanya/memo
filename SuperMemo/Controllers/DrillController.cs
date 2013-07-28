192f54ac81196ac9c837df86ab8da8cdbf4c668d
pxavolkov <a.volkov@svitla.com>
2013-07-28 23:16:29 +0300

    Add stub for training

---
+++ SuperMemo/Controllers/DrillController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+ 
+namespace SuperMemo.Controllers
+{
+    public class DrillController : Controller
+    {
+        //
+        // GET: /Drill/
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+    }
+}
