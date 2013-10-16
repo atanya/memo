4b2f4fe708d8bc4dd37b3cb3bc1710a99297c09d
pxavolkov <a.volkov@svitla.com>
2013-10-16 11:00:39 +0300

    Separate js for edit form, use ko binding for edit form.

---
+++ SuperMemo/Scripts/EditCard.js
@@ -0,0 +1,111 @@
+﻿superMemo.EditCard = {
+    
+    viewModel: {},
+
+    initEditForm: function () {
+        $("#translationInput").autocomplete({
+            source: function (request, response) {
+                var res = "";
+                var rus = "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ.,";
+                var eng = "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>/?";
+                for (var i = 0; i < request.term.length; i++) {
+                    var pos = eng.indexOf(request.term[i]);
+                    res += pos === -1 ? request.term[i] : rus[pos];
+                }
+                response([res]);
+            },
+            select: function (event, ui) {
+                $(this).val(ui.item.value).change();
+            }
+        });
+    },
+    
+    wordKeyPress: function(vm, e) {
+        if (e.which == 13) {
+            if (!vm.translation()) {
+                $('#translationInput').focus();
+            } else {
+                superMemo.EditCard.saveCup();
+            }
+        }
+        return true;
+    },
+
+    translationKeyPress: function (vm, e) {
+        if (e.which == 13) {
+            superMemo.EditCard.saveCup();
+        }
+        return true;
+    },
+
+    saveCup: function () {
+        superMemo.EditCard.showMessage("");
+        var vm = superMemo.EditCard.viewModel;
+        if (!superMemo.EditCard.checkValidation(vm.word(), vm.translation())) {
+            return;
+        }
+        superMemo.proxy.saveCup(vm.id(), vm.word(), vm.translation(), function (result) {
+            superMemo.EditCard.showErrorMessage("Card saved");
+            vm.word("");
+            vm.translation("");
+        }, function (result) {
+            alert("failure");
+        });
+    },
+
+    checkValidation: function (word, translation) {
+        superMemo.EditCard.showMessage("#wordValidation", "");
+        superMemo.EditCard.showMessage("#translationValidation", "");
+        if (!word) {
+            superMemo.EditCard.showMessage("#wordValidation", "Required");
+        }
+        if (!translation) {
+            superMemo.EditCard.showMessage("#translationValidation", "Required");
+        }
+        return !!word && !!translation;
+    },
+
+    showErrorMessage: function (message) {
+        superMemo.EditCard.showMessage("#messageLabel", message);
+    },
+
+    showMessage: function (control, message) {
+        $(control).text(message);
+    },
+    
+    onLoadCardSuccessCallback: function (response) {
+        superMemo.EditCard.applyBindings(ko.mapping.fromJS(response));
+        
+    },
+
+    onLoadFailureCallback: function (response) {
+        alert("Fail");
+    },
+    
+    loadCard: function (id) {
+        if (id) {
+            superMemo.proxy.loadCard(id, superMemo.EditCard.onLoadCardSuccessCallback, superMemo.EditCard.onLoadFailureCallback);
+        } else {
+            superMemo.EditCard.applyBindings(superMemo.EditCard.defaultViewModel());
+        }
+        
+    },
+    
+    defaultViewModel: function() {
+        return {
+            word: ko.observable(""),
+            translation: ko.observable(""),
+            id: ko.observable("")
+        };
+    },
+    
+    applyBindings: function (viewModel) {
+        var self = superMemo.EditCard;
+        self.viewModel = viewModel;
+        self.viewModel.saveCard = self.saveCup;
+        self.viewModel.wordKeyPress = self.wordKeyPress;
+        ko.applyBindings(superMemo.EditCard.viewModel);
+        $("#wordInput").focus();
+    }
+
+}
--- SuperMemo/Scripts/card.js
+++ SuperMemo/Scripts/card.js
@@ -1,75 +1,4 @@
 ﻿superMemo.Card = {
-    
-    initEditForm: function() {
-        $('#wordInput').keypress(function(e) {
-            if (e.which == 13) {
-                if (!$("#translationInput").val()) {
-                    $('#translationInput').focus();
-                } else {
-                    superMemo.Card.saveCup();
-                }
-            }
-        });
-
-        $('#translationInput').keypress(function(e) {
-            if (e.which == 13) {
-                superMemo.Card.saveCup();
-            }
-        });
-
-        $("#saveButton").click(superMemo.Card.saveCup);
-
-        $("#translationInput").autocomplete({
-            source: function (request, response) {
-                var res = "";
-                var rus = "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ";
-                var eng = "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>";
-                for (var i = 0; i < request.term.length; i++) {
-                    var pos = eng.indexOf(request.term[i]);
-                    res += pos === -1 ? request.term[i] : rus[pos];
-                }
-                response([res]);
-            }
-        });
-    },
-    
-    saveCup: function() {
-        superMemo.Card.showMessage("");
-        var word = $("#wordInput").val();
-        var translaton = $("#translationInput").val();
-        var id = $("#WordID").val();
-        if (!superMemo.Card.checkValidation(word, translaton)) {
-            return;
-        }
-        superMemo.proxy.saveCup(id, word, translaton, function(result) {
-            superMemo.Card.showErrorMessage("Card saved");
-            $("#wordInput").val("");
-            $("#translationInput").val("");
-        }, function(result) {
-            alert("failure");
-        });
-    },
-
-    checkValidation: function(word, translation) {
-        superMemo.Card.showMessage("#wordValidation", "");
-        superMemo.Card.showMessage("#translationValidation", "");
-        if (!word) {
-            superMemo.Card.showMessage("#wordValidation", "Required");
-        }
-        if (!translation) {
-            superMemo.Card.showMessage("#translationValidation", "Required");
-        }
-        return !!word && !!translation;
-    },
-
-    showErrorMessage: function(message) {
-        superMemo.Card.showMessage("#messageLabel", message);
-    },
-
-    showMessage: function(control, message) {
-        $(control).text(message);
-    },
-
     viewModel: {
         cards: [],
         
@@ -91,22 +20,12 @@
         updateCup: function(func, event) {
             var id = superMemo.Card.viewModel.getCardID(event.currentTarget);
             window.location = "Card/Edit/" + id;
-        }/*,
-
-        addCup: function() {
-            window.location = "Create";
-        }*/
+        }
     },
 
     onLoadSuccessCallback: function(response) {
         superMemo.Card.viewModel.cards = response;
         ko.applyBindings(superMemo.Card.viewModel, document.getElementById('cardList'));
-    },
-
-    onLoadCardSuccessCallback: function (response) {
-        $("#wordInput").val(response.word);
-        $("#translationInput").val(response.translation);
-        $("#wordInput").focus();
     },
 
     onLoadFailureCallback: function(response) {
@@ -115,10 +34,6 @@
 
     load: function() {
         superMemo.proxy.loadList(superMemo.Card.onLoadSuccessCallback, superMemo.Card.onLoadFailureCallback);
-    },
-
-    loadCard: function (id) {
-        superMemo.proxy.loadCard(id, superMemo.Card.onLoadCardSuccessCallback, superMemo.Card.onLoadFailureCallback);
     }
 };
 superMemo.Card.load();
